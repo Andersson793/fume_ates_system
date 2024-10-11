0a5b46c281933f33f08e098d0ba0e83e77f8a93c
Andersson S. Gonçalves <anderssonsilva793@gmail.com>
2024-10-11 20:02:46 -0300

    update AlertItem.vue and index.vue components

--- components/AlertItem.vue
+++ components/AlertItem.vue
@@ -1,41 +1,38 @@
 <script>
-import { X } from 'lucide-vue-next';
-
+import { X } from "lucide-vue-next";
 
 export default {
-    
     props: {
         user: {
             type: String,
-            default: 'user undefined'
+            default: "user undefined",
         },
         date: {
             type: String,
         },
         body: {
             type: String,
-            default: ''
-        }
+            default: "",
+        },
     },
-    components: {X}
-}
+    components: { X },
+};
 </script>
 <template>
-    <div class='bg-green-200 p-1 rounded-sm'>
-        <div class='flex justify-end items-center py-1.5 text-sm'>
+    <div class="bg-green-200 p-1 rounded-sm mb-10">
+        <div class="flex justify-end items-center py-1.5 text-sm">
             <span>
                 {{ $props.date }}
             </span>
-            <span class='mx-5'>
+            <span class="mx-5">
                 {{ $props.user }}
             </span>
-            <span class='cursor-pointer'>
-                <X size='22'/>
+            <span class="cursor-pointer">
+                <X size="22" />
             </span>
         </div>
-        <div class='p-2 bg-pink-200 rounded-md'>
-            <p>{{ $props.text }}</p>
+        <div class="p-2 bg-blue-200 rounded-md">
+            <p>{{ $props.body }}</p>
         </div>
-        
     </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,48 @@
+<script>
+export default {
+    data() {
+        return {
+            alerts: [
+                {
+                    user: "andersson793",
+                    date: "12/12/2001 10:23",
+                    body: "Any text !!!!",
+                },
+                {
+                    user: "andersson793",
+                    date: "12/12/2001 10:23",
+                    body: "Any text !!!!",
+                },
+                {
+                    user: "andersson793",
+                    date: "12/12/2001 10:23",
+                    body: "Any text !!!!",
+                },
+                {
+                    user: "andersson793",
+                    date: "12/12/2001 10:23",
+                    body: "Any text !!!!",
+                },
+                {
+                    user: "andersson793",
+                    date: "12/12/2001 10:23",
+                    body: "Any text !!!!",
+                },
+                {
+                    user: "andersson793",
+                    date: "12/12/2001 10:23",
+                    body: "Any text !!!!",
+                },
+                {
+                    user: "andersson793",
+                    date: "12/12/2001 10:23",
+                    body: "Any text !!!!",
+                },
+            ],
+        };
+    },
+};
+</script>
 <template>
     <AppMain>
         <AppPanel title_panel="Title panel" class="col-span-5">
@@ -5,14 +50,16 @@
         </AppPanel>
 
         <AppPanel title_panel="Alerts" class="col-span-3">
-
-            <div v-if='true'>
-                <AlertItem/>
+            <div class="overflow-y-scroll" v-if="alerts.length > 0">
+                <AlertItem
+                    v-for="alert in alerts"
+                    :user="alert.user"
+                    :date="alert.date"
+                    :body="alert.body"
+                />
             </div>
 
-            <div v-else class="h-max w-max bg-gray-200 rounded-md">
-                Nothing here !
-            </div>
+            <div v-else class="text-center font-bold">Nothing here !</div>
         </AppPanel>
     </AppMain>
 </template>
