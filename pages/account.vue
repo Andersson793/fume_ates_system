b3a5c2453dd802c6d68bfe9f827d10890343f68f
Andersson S. Gonçalves <anderssonsilva793@gmail.com>
2024-10-16 20:05:26 -0300

    update maneger page and other changes

--- pages/account.vue
+++ pages/account.vue
@@ -17,43 +17,19 @@ export default {
     <AppMain>
         <AppPanel title_panel="User information" class="col-span-3">
             <div>
-                <div class='py-5'>
+                <div class="py-5">
                     <span>User name</span>
-                    <p class='text-lg'>andersson793</p>
-
+                    <p class="text-lg">andersson793</p>
                 </div>
-                <div class='py-5'>
+                <div class="py-5">
                     <span>Email</span>
-                    <p class='text-lg'>andersson793@gmail.com</p>
-
+                    <p class="text-lg">andersson793@gmail.com</p>
                 </div>
-                <div class='py-5'>
+                <div class="py-5">
                     <span>Account</span>
-                    <p class='text-lg'>type: Master</p>
-                    <p class='text-lg'>last login: 12/12/2001 10:15</p>
-
+                    <p class="text-lg">type: Master</p>
+                    <p class="text-lg">last login: 12/12/2001 10:15</p>
                 </div>
-                
-            </div>
-        </AppPanel>
-        <AppPanel title_panel="Password" class="col-span-3">
-            <div>
-                <span>Change Password</span>
-                <div class="pl-10 mb-16">
-                    <AppInput
-                    
-                        value=""
-                        label="New password"
-                        type='password'
-                    />
-                    <AppInput
-                        
-                        value=""
-                        label="Repeat"
-                        type='password'
-                    />
-                </div>
-                <AppButton>Chenge</AppButton>
             </div>
         </AppPanel>
     </AppMain>
